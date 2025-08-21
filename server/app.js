4621ec0dee41002b7afb45de9c282e7b7f885a3d
meghana610 <devatameghana610@gmail.com>
2025-08-21 11:51:38 +0530

    postman practice

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 const app = express();
-app.use(express.json()) //This line imp for post,put,delete api's
+
+app.use(express.json()) //This line imp for post,put,delete api's and not for GET
 
 const PORT = 5002;
 
@@ -13,13 +14,33 @@ app.get("/", (req, res) => {
   res.send("This is meghana");
 });
 
-app.post("/details",(req,res)=>{
-  let userData = req.body
-  console.log(userData)
-});
+// app.get("/global", (req, res) => {
+//   res.send("Hello Global");
+//     res.json(userData)
+// });
 
+
+// app.post("/details",(req,res)=>{
+//   let userData = req.body
+//   console.log(userData)
+//   res.json(userData)
+// });
+
+
+// app.post("/students"/"22U61A0513",(req,res) => {
+//     res.send("Hello Meghana")
+// });
+
+//req.params
+
+app.post("/students/:id",(req,res)=>{
+    let id = req.params.id;
+    console.log(id)
+    res.json(id)
+})
 
 // Start server
+
 app.listen(PORT, () => {
   console.log(` Server running at http://localhost:${PORT}`);
 });
